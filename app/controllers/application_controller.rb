dd0cbb4bb699a09ae001b8f0f96e20d1459b377c
daudsd <daudsadiq1@gmail.com>
2022-03-07 19:18:17 -0800

    name attribute saved in user model, devise view updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   Pagy::DEFAULT[:items] = 10      # items per page
   Pagy::DEFAULT[:size]  = [1,4,4,1] # nav bar links
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  private
+
+  #accepting name (additional attribute) for create/update
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end

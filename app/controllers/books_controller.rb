306fba3716a4e5ac61fd8bb9b8b52c40cd780e3f
daudsd <daudsadiq1@gmail.com>
2022-03-08 14:29:15 -0800

    quantity added in books, search forms updated, books table actions updated according to user role

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -63,6 +63,6 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       print params
-      params.require(:book).permit(:title, :publisher, :isbn, :published_date)
+      params.require(:book).permit(:title, :publisher, :isbn, :published_date, :quantity)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,9 @@ class Book < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :publisher
 
-  before_validation :set_published_date, unless: -> { published_date.present? }
+  before_validation :set_data, unless: -> { published_date.present? }
 
-  scope :ordered, -> { order(:created_at, :title) }
+  scope :ordered, -> { order(created_at: :desc, title: :asc) }
 
   def self.search(params)
     books = Book.all
@@ -12,12 +12,14 @@ class Book < ApplicationRecord
     books = Book.where('publisher ILIKE ?', "#{params[:publisher]}%") if params[:publisher].present?
     books = Book.where(isbn: params[:isbn]) if params[:isbn].present?
     books = Book.where(published_date: params[:published_date]) if params[:published_date].present?
+    books = Book.where(quantity: params[:quantity]) if params[:quantity].present?
     books.distinct.ordered
   end
 
   private
 
-  def set_published_date
-    self.published_date = Time.now
+  def set_data
+    self.published_date = Time.now unless published_date.present?
+    self.quantity = 0 unless quantity.present?
   end
 end
